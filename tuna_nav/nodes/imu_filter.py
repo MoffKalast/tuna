434568ac36c334ce20a4a053822cec7b456e6391
MoffKalast <vid.rijavec@gmail.com>
2023-05-20 23:03:25 +0000

    imu filter fixes

--- tuna_nav/nodes/imu_filter.py
+++ tuna_nav/nodes/imu_filter.py
@@ -20,18 +20,18 @@ class IMUFilter:
 		self.mag_angle_x = 0
 		self.mag_angle_y = 0
 		self.mag_angle_z = 0
+		self.gyro_callib_y = None
 
 		self.rotation = (0.0, 0.0, 0.0, 1.0)
 		self.gps_rotation = None
 
 		self.imu_pub = rospy.Publisher("imu/data", Imu, queue_size=50)
 
-		self.imu_sub = rospy.Subscriber("imu/data_raw", Imu, self.imu_data)
+		self.imu_sub = rospy.Subscriber("imu/raw", Imu, self.imu_data)
 		self.mag_sub = rospy.Subscriber("imu/mag", MagneticField, self.mag_data)
 		self.gps_sub = rospy.Subscriber("navsat_simple/heading", PoseWithCovarianceStamped, self.gps_data)
 
 		self.pose_pub = rospy.Publisher('imu/debug_pose', PoseWithCovarianceStamped, queue_size=10)
-
 
 	def gps_data(self, msg):
 		q = msg.pose.pose.orientation
@@ -40,8 +40,6 @@ class IMUFilter:
 			self.gps_rotation = None
 		else:		
 			self.gps_rotation = [q.x, q.y, q.z, q.w]
-
-		self.gyro_callib_y = None
 
 	def mag_data(self, msg):
 		m = msg.magnetic_field
