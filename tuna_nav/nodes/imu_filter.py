21d84903f8d5197097ecac01d2a91e2f77525dfc
MoffKalast <vid.rijavec@gmail.com>
2023-05-20 23:24:31 +0200

    gps heading injection

--- tuna_nav/nodes/imu_filter.py
+++ tuna_nav/nodes/imu_filter.py
@@ -18,12 +18,25 @@ class IMUFilter:
 
 		self.mag_angle = 0
 		self.rotation = (0.0, 0.0, 0.0, 1.0)
+		self.gps_rotation = None
 		self.last_time = rospy.Time.now().to_sec()
 
-		self.imu_pub = rospy.Publisher("/imu/data", Imu, queue_size=50)
+		self.imu_pub = rospy.Publisher("imu/data", Imu, queue_size=50)
 
-		self.imu_sub = rospy.Subscriber("/imu/raw", Imu, self.imu_data)
-		self.mag_sub = rospy.Subscriber("/imu/mag", MagneticField, self.mag_data)
+		self.imu_sub = rospy.Subscriber("imu/data_raw", Imu, self.imu_data)
+		self.mag_sub = rospy.Subscriber("imu/mag", MagneticField, self.mag_data)
+		self.gps_sub = rospy.Subscriber("navsat_simple/heading", PoseWithCovarianceStamped, self.gps_data)
+
+		self.pose_pub = rospy.Publisher('imu/debug_pose', PoseWithCovarianceStamped, queue_size=10)
+
+
+	def gps_data(self, msg):
+		q = msg.pose.pose.orientation
+
+		if q.x == 0 and q.y == 0 and q.z == 0 and q.w == 0:
+			self.gps_rotation = None
+		else:		
+			self.gps_rotation = [q.x, q.y, q.z, q.w]
 
 	def mag_data(self, msg):
 		m = msg.magnetic_field
@@ -55,6 +68,9 @@ class IMUFilter:
 		magquat = tf.transformations.quaternion_from_euler(0, 0, self.mag_angle)
 		self.rotation = tf.transformations.quaternion_slerp(self.rotation, magquat, 0.02)
 
+		if not self.gps_rotation is None:
+			self.rotation = tf.transformations.quaternion_slerp(self.rotation, self.gps_rotation, 0.04)
+
 		msg.orientation.x = self.rotation[0]
 		msg.orientation.y = self.rotation[1]
 		msg.orientation.z = self.rotation[2]
@@ -67,8 +83,6 @@ class IMUFilter:
 		test.pose.pose.orientation = msg.orientation
 		self.pose_pub.publish(test)
 
-try:
-	b = IMUFilter()
-	rospy.spin()
-except rospy.ROSInterruptException:
-	print("Script interrupted", file=sys.stderr)
+
+b = IMUFilter()
+rospy.spin()
